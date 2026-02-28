0e408b2079ac847fa831195eb31d7b68059f6007
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-02-28 12:32:19 +0530

    Multilevel inheritance

--- Oops/Oops.java
+++ Oops/Oops.java
@@ -1,10 +1,14 @@
 package Oops;
 
+import java.util.Optional;
+
 public class Oops {
     public static void main(String[] args) {
 
         B b = new B("Rohit");
         b.printName();
+        C c = new C("Amit");
+        c.getFatherName();
 
     }
 }
@@ -26,5 +30,17 @@ class B extends A {
 
     void printName() {
         System.out.println(" your name is " + giveName(name));
+    }
+}
+
+class C extends A {
+    String fatherName;
+
+    C(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    void getFatherName() {
+        System.out.println("your father name is : " + giveName(fatherName));
     }
 }
