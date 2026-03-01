0d7a73301a6760dfaa4e98c215b39027b03c5ad3
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-03-01 08:45:22 +0530

    Abstraction ans Encapsulation

--- Oops/AbstractClass.java
+++ Oops/AbstractClass.java
@@ -1,11 +1,41 @@
 package Oops;
 
+import java8.StreamAPIs.mapOperation;
+
 public class AbstractClass {
     public static void main(String[] args) {
 
         Main.hello();
         test.hello();
+
+        Encapsulation user = new Encapsulation();
+        user.TrasnderMoney(900, 11);
+        System.out.println(user.getBalance());
     }
+}
+
+class Encapsulation {
+    private int balance = 9000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void TrasnderMoney(int money, int transferId) {
+        if (money <= 0) {
+            System.out.println("Sorry 0 money cannot  be send");
+        } else if (transferId < 100) {
+            if (balance < money) {
+                System.out.println("Sorry your account not suficient balance");
+            } else {
+                balance -= money;
+                System.out.println("Money transfer successfull");
+            }
+        } else {
+            System.out.println("Id is envalid");
+        }
+    }
+
 }
 
 abstract class Main {
