7b39ab94631d7c184c8343e660f901e4721826a3
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-03-18 08:28:10 +0530

    lock object level per lagta hia hai , ager different object pe do alag alag thread to vo dono parallel chalenge kyoki , lock object level per lagta hai

---
+++ multhreading/Question4.java
@@ -0,0 +1,50 @@
+package multhreading;
+
+class Helper1 {
+    public void method1() {
+        synchronized (this) {
+            System.out.println("Method 1");
+        }
+    }
+
+    public void method2() {
+        synchronized (this) {
+            System.out.println("Method 2");
+        }
+    }
+}
+
+public class Question4 {
+
+    public static void main(String[] args) {
+
+        Helper1 helper = new Helper1();
+
+        Thread t1 = new Thread(() -> {
+            helper.method1();
+        });
+
+        Thread t2 = new Thread(() -> {
+            helper.method2();
+        });
+
+        // t1.start();
+        // t2.start();
+
+        Helper2 call1 = new Helper2();
+        Helper2 call2 = new Helper2();
+
+        call1.method1();
+        call2.method2();
+    }
+}
+
+class Helper2 extends Thread {
+    public void method1() {
+        System.out.println("Method 1 , from Helper 2 class");
+    }
+
+    public void method2() {
+        System.out.println("Method 2 , from Helper 2 class");
+    }
+}
